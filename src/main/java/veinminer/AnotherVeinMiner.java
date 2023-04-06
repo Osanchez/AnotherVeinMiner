6aae503eaf0394b0d31e5d45226fd4eeeca29dd3
son_goku86 <26527907+MikeRohsoft@users.noreply.github.com>
2023-04-06 23:44:31 +0200

    updated to Game Version 0.21.29

--- src/main/java/veinminer/AnotherVeinMiner.java
+++ src/main/java/veinminer/AnotherVeinMiner.java
@@ -1,16 +1,19 @@
 package veinminer;
 import necesse.engine.control.Control;
+import necesse.engine.control.Control.ControlGroup;
 import necesse.engine.modLoader.annotations.ModEntry;
 import necesse.engine.registries.PacketRegistry;
 import veinminer.objects.Config;
 import veinminer.packets.PacketObjectsDestroyed;
 import veinminer.utils.ConfigParser;
-
+import necesse.engine.localization.message.LocalMessage;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.HashSet;
+
+import javax.naming.ldap.ControlFactory;
 
 import static veinminer.utils.ModMisc.getModVersion;
 
@@ -49,12 +52,12 @@ public class AnotherVeinMiner {
             Class<Control> classObj = Control.class;
 
             //create an instance of control class
-            Constructor<? extends Control> controlConstructor = classObj.getDeclaredConstructor(Integer.TYPE, String.class, Integer.TYPE);
+            Constructor<? extends Control> controlConstructor = classObj.getDeclaredConstructor(Integer.TYPE, String.class);
             controlConstructor.setAccessible(true);
             //get the add control method
-            Method addControlMethod = classObj.getDeclaredMethod("addControl", Control.class);
+            Method addControlMethod = classObj.getDeclaredMethod("addControl", Control.class, ControlGroup.class);
             addControlMethod.setAccessible(true);
-            SPEED_MINE = (Control) addControlMethod.invoke(null, controlConstructor.newInstance(miningCharKeyCode, "speedmine", 1));
+            SPEED_MINE = (Control) addControlMethod.invoke(null, controlConstructor.newInstance(miningCharKeyCode, "speedmine"), new ControlGroup(1, new LocalMessage("controls", "groupgeneral")));
 
         } catch (Exception e) {
             System.out.println("Could not create fast mine key bind");
