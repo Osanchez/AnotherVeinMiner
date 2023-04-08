3a223cdb6a82ee5242468947034d9649c22d01f3
son_goku86 <26527907+MikeRohsoft@users.noreply.github.com>
2023-04-08 14:54:47 +0200

    Replace for basically everything
    Changes BFS to DFS
    Replaces JSON with Necesse Config format
    Replaces JSON Transport with raw data
    Squashes Keys and Coordinates
    Removes all external dependencies

--- src/main/java/veinminer/AnotherVeinMiner.java
+++ src/main/java/veinminer/AnotherVeinMiner.java
@@ -5,53 +5,26 @@ import necesse.engine.modLoader.annotations.ModEntry;
 import necesse.engine.registries.PacketRegistry;
 import veinminer.objects.Config;
 import veinminer.packets.PacketObjectsDestroyed;
-import veinminer.utils.ConfigParser;
 import necesse.engine.localization.message.LocalMessage;
 import java.awt.event.KeyEvent;
-import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
-import java.util.HashSet;
-
-import javax.naming.ldap.ControlFactory;
-
-import static veinminer.utils.ModMisc.getModVersion;
 
 @ModEntry
 public class AnotherVeinMiner {
 
     public static Control SPEED_MINE;
-    public static Config modConfig;
-    public static boolean configReadAttempted = false;
-    public static HashSet<String> oreIDs;
-    public static int radius;
-
+    public static int RADIUS;
     public AnotherVeinMiner() {
-        readConfigFile();
-        configureMiningControl();
-    }
-
-    public static void readConfigFile() {
-        ConfigParser configReader = new ConfigParser();
-        configReadAttempted = true;
-        try {
-            modConfig = configReader.parseConfig();
-            oreIDs = modConfig.getOreIDs();
-            radius = modConfig.get_radius();
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+        //get the mining key to use
+        AnotherVeinMiner.RADIUS = Config.getRadius();
+        Character miningChar = Config.getMiningKey();
+        if (miningChar == null) {
+            SPEED_MINE = null;
+            return;
         }
-    }
-
-    public void configureMiningControl() {
+        int miningCharKeyCode = KeyEvent.getExtendedKeyCodeForChar(miningChar);
         try {
-            //get the mining key to use
-            Character miningChar = modConfig.get_mining_key();
-            if (miningChar == null) {
-                SPEED_MINE = null;
-            }
-            int miningCharKeyCode = KeyEvent.getExtendedKeyCodeForChar(miningChar);
-
             Class<Control> classObj = Control.class;
 
             //create an instance of control class
@@ -61,7 +34,6 @@ public class AnotherVeinMiner {
             Method addControlMethod = classObj.getDeclaredMethod("addControl", Control.class, ControlGroup.class);
             addControlMethod.setAccessible(true);
             SPEED_MINE = (Control) addControlMethod.invoke(null, controlConstructor.newInstance(miningCharKeyCode, "speedmine"), new ControlGroup(1, new LocalMessage("controls", "groupgeneral")));
-
         } catch (Exception e) {
             System.out.println("Could not create fast mine key bind");
         }
@@ -69,7 +41,6 @@ public class AnotherVeinMiner {
 
     public void init() {
         PacketRegistry.registerPacket(PacketObjectsDestroyed.class);
-        System.out.printf("AnotherVeinMiner (version %s) by Trihardest Loaded!\n", getModVersion());
     }
 
 }
--- src/main/java/veinminer/packets/PacketObjectsDestroyed.java
+++ src/main/java/veinminer/packets/PacketObjectsDestroyed.java
@@ -1,11 +1,11 @@
 package veinminer.packets;
 
 import java.awt.*;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 import necesse.engine.GameEvents;
 import necesse.engine.events.loot.ObjectLootTableDropsEvent;
@@ -23,54 +23,42 @@ import necesse.inventory.InventoryItem;
 import necesse.level.gameObject.GameObject;
 import necesse.level.maps.Level;
 import necesse.level.maps.LevelObject;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import veinminer.AnotherVeinMiner;
 import veinminer.objects.Coordinate;
-import veinminer.utils.CoordinateParser;
+import veinminer.objects.Config;
 
 public class PacketObjectsDestroyed extends Packet {
-    public final ArrayList<Coordinate> tiles;
 
+    public final ArrayList<Coordinate> tiles;
+    public static Set<String> ores = new HashSet<>(Arrays.asList(Config.getOres()));
     public PacketObjectsDestroyed(byte[] data) {
         super(data);
         PacketReader reader = new PacketReader(this);
-        String rawJson = reader.getNextString();
-        CoordinateParser parser = new CoordinateParser();
-        ArrayList<Coordinate> tiles;
-        try {
-            tiles = parser.parseCoordinates(rawJson);
-        } catch (IOException e) {
-            System.out.printf("Failed to parse PacketObjectsDestroyed packet, Error: %s", e.getMessage());
-            tiles = new ArrayList<>();
-        }
+        ArrayList<Coordinate> tiles = new ArrayList<>();
+        while (reader.hasNext()) 
+            tiles.add(new Coordinate(reader.getNextInt(), reader.getNextInt(), reader.getNextInt()));
+
         this.tiles = tiles;
     }
 
     public PacketObjectsDestroyed(ArrayList<Coordinate> all_tiles) {
         this.tiles = all_tiles;
         PacketWriter writer = new PacketWriter(this);
-        //convert coordinates of tiles into string json
-        JSONObject tilesJSON = new JSONObject();
-        JSONArray tiles = new JSONArray();
-        for(Coordinate coordinate : all_tiles) {
-            tiles.add(coordinate.getJSON());
+        for (Coordinate a : all_tiles) {
+            writer.putNextInt(a.x);
+            writer.putNextInt(a.y);
+            writer.putNextInt(a.gameID);
         }
-        tilesJSON.put("tiles", tiles);
-        writer.putNextString(tilesJSON.toJSONString());
     }
 
-    //copied from GameObject.java, avoid invocation of patched method
     public void onDestroyed(GameObject gameObject, Level level, int x, int y, ServerClient client, ArrayList<ItemPickupEntity> itemsDropped) {
         if (itemsDropped != null) {
             ArrayList<InventoryItem> drops = gameObject.getDroppedItems(level, x, y);
             ObjectLootTableDropsEvent dropsEvent;
             GameEvents.triggerEvent(dropsEvent = new ObjectLootTableDropsEvent(new LevelObject(level, x, y), new Point(x * 32 + 16, y * 32 + 16), drops));
             if (dropsEvent.dropPos != null && dropsEvent.drops != null) {
-                Iterator var8 = dropsEvent.drops.iterator();
-
+                Iterator<InventoryItem> var8 = dropsEvent.drops.iterator();
                 while(var8.hasNext()) {
-                    InventoryItem item = (InventoryItem)var8.next();
+                    InventoryItem item = var8.next();
                     ItemPickupEntity droppedItem = item.getPickupEntity(level, (float)dropsEvent.dropPos.x, (float)dropsEvent.dropPos.y);
                     level.entityManager.pickups.add(droppedItem);
                     itemsDropped.add(droppedItem);
@@ -78,59 +66,43 @@ public class PacketObjectsDestroyed extends Packet {
             }
         }
 
-        if (client != null) {
-            client.newStats.objects_mined.increment(1);
-        }
-
-        if (!level.isServerLevel()) {
+        if (!level.isServerLevel())
             gameObject.spawnDestroyedParticles(level, x, y);
-        }
 
         ObjectEntity objectEntity = level.entityManager.getObjectEntity(x, y);
         level.setObject(x, y, 0);
-        if (objectEntity != null) {
+        if (objectEntity != null) 
             objectEntity.remove();
-        }
-
     }
 
 
     public void processServer(NetworkPacket packet, Server server, ServerClient serverClient) {
         ArrayList<Coordinate> destroyOnClient = new ArrayList<>();
-        for(Coordinate coordinate : this.tiles) {
-            int tileX = coordinate.getX();
-            int tileY = coordinate.getY();
-            int tileID = coordinate.getID();
-            if(serverClient != null) {
-                if (serverClient.getLevel() != null) {
-                    LevelObject serverLevelObject = serverClient.getLevel().getLevelObject(tileX, tileY);
-                    if (serverLevelObject.object.getID() == tileID && AnotherVeinMiner.oreIDs.contains(serverLevelObject.object.getStringID())) {
-                        // remove level object from server
-                        onDestroyed(serverLevelObject.object, serverLevelObject.level, tileX, tileY, serverClient, new ArrayList<>());
-                        destroyOnClient.add(coordinate);
-                    }
-                }
-            }
+        for (Coordinate coordinate : this.tiles) {
+            if (serverClient == null || serverClient.getLevel() == null)
+                continue;
+
+            LevelObject serverLevelObject = serverClient.getLevel().getLevelObject(coordinate.x, coordinate.y);
+            if (!ores.contains(serverLevelObject.object.getStringID())) 
+                continue;
+
+            onDestroyed(serverLevelObject.object, serverLevelObject.level, coordinate.x, coordinate.y, serverClient, new ArrayList<>());
+            destroyOnClient.add(coordinate);
         }
-        if(destroyOnClient.size() > 0) {
+        if (destroyOnClient.size() > 0)
             server.network.sendToClientsAt(new PacketObjectsDestroyed(destroyOnClient), serverClient.getLevel());
-        }
     }
 
     public void processClient(NetworkPacket packet, Client client) {
-        for(Coordinate coordinate : this.tiles) {
-            int tileX = coordinate.getX();
-            int tileY = coordinate.getY();
-            int tileID = coordinate.getID();
+        for (Coordinate coordinate : this.tiles) {
+            if (client.getLevel() == null) 
+                continue;
 
-            if (client.getLevel() != null) {
-                LevelObject lo = client.getLevel().getLevelObject(tileX, tileY);
-                if (lo.object.getID() == tileID) {
-                    onDestroyed(lo.object, lo.level, tileX, tileY, null, null);
-                } else {
-                    client.network.sendPacket(new PacketRequestObjectChange(tileX, tileY));
-                }
-            }
+            LevelObject lo = client.getLevel().getLevelObject(coordinate.x, coordinate.y);
+            if (lo.object.getID() == coordinate.gameID)
+                onDestroyed(lo.object, lo.level, coordinate.x, coordinate.y, null, null);
+            else
+                client.network.sendPacket(new PacketRequestObjectChange(coordinate.x, coordinate.y));
         }
     }
 }
